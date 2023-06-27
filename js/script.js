689649c974c05f21166563ef6b85722e4a153696
rafasilvadev <rafabdasilva12@gmail.com>
2023-06-27 18:24:02 -0300

    Responsive header applied.

--- js/script.js
+++
@@ -1,5 +0,0 @@
-function toggleDropdown() {
-  var dropdownContent = document.querySelector(".dropdown-content");
-  dropdownContent.style.display =
-    dropdownContent.style.display === "block" ? "none" : "block";
-}
