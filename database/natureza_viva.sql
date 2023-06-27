1bb5e0540aa44094f17446e083fa4570b8d4c881
Rafael Barbosa da Silva <73250106+RafaSilvaDev@users.noreply.github.com>
2023-06-27 07:16:47 -0300

    Updating database

--- database/natureza_viva.sql
+++ database/natureza_viva.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21-Jun-2023 às 12:41
--- Versão do servidor: 10.4.19-MariaDB
--- versão do PHP: 7.3.28
+-- Generation Time: Jun 27, 2023 at 12:16 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `natureza_viva`
+-- Database: `natureza_viva`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `reserva`
+-- Table structure for table `reserva`
 --
 
 CREATE TABLE `reserva` (
@@ -32,86 +32,79 @@ CREATE TABLE `reserva` (
   `data_reserva` date DEFAULT NULL,
   `confirmada` int(11) DEFAULT NULL,
   `locador` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reserva`
+--
+
+INSERT INTO `reserva` (`id`, `data_reserva`, `confirmada`, `locador`) VALUES
+(1, '2023-06-21', 0, 3),
+(4, '2023-06-22', 0, 3);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Table structure for table `usuario`
 --
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `senha` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabela de usuários comuns do sistema';
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario_adm`
---
-
-CREATE TABLE `usuario_adm` (
-  `id` int(5) NOT NULL,
-  `nome` varchar(100) NOT NULL,
-  `email` text NOT NULL,
   `senha` varchar(200) NOT NULL,
-  `primeiro_acesso` int(2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Tabela de usuários administradores';
+  `tipo` enum('cliente','adm') NOT NULL,
+  `primeiro_acesso` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabela de usuários comuns do sistema';
 
 --
--- Índices para tabelas despejadas
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `tipo`, `primeiro_acesso`) VALUES
+(2, '', '', '$2y$10$nR3hPsOgRknVl0/12rcWheEkdvQt6DJhgUNwroLvpOxJVvdse5i6C', 'cliente', 0),
+(3, 'rafa', 'rafa@gmail.com', 'MTIz', 'cliente', 0),
+(4, 'admin', 'admin@admin.com', 'MTIzNDU2', 'adm', 1);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `reserva`
+-- Indexes for table `reserva`
 --
 ALTER TABLE `reserva`
   ADD PRIMARY KEY (`id`),
   ADD KEY `locador` (`locador`);
 
 --
--- Índices para tabela `usuario`
+-- Indexes for table `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `usuario_adm`
---
-ALTER TABLE `usuario_adm`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `reserva`
+-- AUTO_INCREMENT for table `reserva`
 --
 ALTER TABLE `reserva`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de tabela `usuario`
+-- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de tabela `usuario_adm`
---
-ALTER TABLE `usuario_adm`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
-
---
--- Restrições para despejos de tabelas
+-- Constraints for dumped tables
 --
 
 --
--- Limitadores para a tabela `reserva`
+-- Constraints for table `reserva`
 --
 ALTER TABLE `reserva`
   ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`locador`) REFERENCES `usuario` (`id`);
